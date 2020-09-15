9122cde09f5c901f698f19eb71c64a768aed9fbf
Olaf Hochherz <olaf@klingt.org>
2020-09-16 00:37:14 +0800

    use dependencies from git

--- mix.exs
+++ mix.exs
@@ -28,16 +28,12 @@ defmodule Axotypixusc.MixProject do
   defp deps do
     [
       {:dialyxir, "~> 1.0", only: [:dev], runtime: false},
-      # {:dep_from_hexpm, "~> 0.3.0"},
-      # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
-      # {:portmidi, git: "https://github.com/zenwerk/ex-portmidi.git", override: true},
-      # {:portmidi, git: "https://github.com/Kovak/ex-portmidi", override: true},
-      # {:sc_ex_scsynth, git: "https://github.com/olafklingt/sc_ex_scsynth"}
-      # {:sc_ex_synthdef, git: "https://github.com/olafklingt/sc_ex_synthdef"}
-      # {:sc_ex_scsynth, path: "../../github/sc_ex_scsynth/"},
-      {:sc_ex_scsoundserver, path: "../../github/sc_ex_scsoundserver/"},
-      {:sc_ex_synthdef, path: "../../github/sc_ex_synthdef/"},
-      {:portmidi, path: "../../github/ex-portmidi/"}
+      {:portmidi, git: "https://github.com/olafklingt/ex-portmidi"},
+      {:sc_ex_scsoundserver, git: "https://github.com/olafklingt/sc_ex_scsoundserver"},
+      {:sc_ex_synthdef, git: "https://github.com/olafklingt/sc_ex_synthdef"}
+      # {:sc_ex_scsoundserver, path: "../../github/sc_ex_scsoundserver/"},
+      # {:sc_ex_synthdef, path: "../../github/sc_ex_synthdef/"},
+      # {:portmidi, path: "../../github/ex-portmidi/"}
     ]
   end
 end
