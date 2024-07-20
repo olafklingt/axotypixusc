9db144d83ce29839f38eeb1f9b43f743f00e7128
olaf <olaf@klingt.org>
2024-07-21 01:09:13 +0200

    nix shell sc dep

--- shell.nix
+++ shell.nix
@@ -3,6 +3,6 @@
 pkgs.mkShell {
   nativeBuildInputs = [  ];
 #  buildInputs = with pkgs; [ elixir portmidi supercollider];
-  buildInputs = with pkgs; [ elixir portmidi ];
+  buildInputs = with pkgs; [ elixir portmidi supercollider];
   LD_LIBRARY_PATH = pkgs.lib.makeLibraryPath [ pkgs.portmidi pkgs.pipewire.jack];
 }
